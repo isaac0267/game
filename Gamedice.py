50f36c94a4c838a2cca67696027576b4b115d20d
Isaac Levy <isaa0267@stud.kea.dk>
2024-10-30 11:11:33 +0100

    The new code is done

--- Gamedice.py
+++ Gamedice.py
@@ -1,26 +1,35 @@
-import random  # Import random libary
-def rolls(): # function, that responsible for rolling the dice. 
-  genrate=random.randint(1,6) # the genrate number will be store in genrate variable. 
-                              # Return the genrate number  
-  return genrate  #If we do not make return the value will be non. 
-print("The genrate number: ", rolls()) #if we want to see the number that been genrate we use print function. 
+import random #Import random libary
 
-#make the vairable 
+print("Welcome to the Dice Game")
+
+def rolls(): # function, that responsible for rolling the dice. 
+ genrate=random.randint(1,6)
+ return genrate #If we do not make return the value will be non. 
+#("Test the number ",rolls()) #This code wil help os to see the randome number.  
+
 player_score=0 # for the player 
 computer_score=0 #The computer variable 
-target_score=20   #Variable for score 
+target_score=20  #Variable for score 
 
-# while loop
-while player_score < target_score and computer_score < target_score: # so long the player_score is less than target_score and computer_score is less than target score
+def playr(): # make a function showing the player win, when it reallay win
+  print("The player win")
+
+def ai():  # make the function the AI is win, when it really win
+  print("AI win the")
+
+
+while player_score<target_score and computer_score<target_score: # so long the player_score is less than target_score and computer_score is less than target score
   player_score+=rolls() # this code is calling the rolls function and opdating the player_score 
   computer_score+=rolls() #The code is doing the same thing. calling the function and adding the value to computer_score 
-  if player_score>=target_score: #Condition, the code is if the player_score is greater or equal to target_score 
-    print("The player is win", str(player_score)) #That mean the player is will win
-    break #If the player win the loop will end 
-  elif computer_score>=target_score: # otherwise the computer is greater or equal to target_score 
-   print("The computer is win: ",str(computer_score)) #The computer will win. cl
-  
 
+  if player_score >=target_score:  #Condition, the code is if the player_score is greater or equal to target_score
+   playr() # calling the win function
+   break # make a break if the player win 
+  elif computer_score>=target_score: ## otherwise the computer is greater or equal to target_score 
+    ai() #calling the ai function. 
+    break  # the loop will stop if the ai winclear
+  
+  
 
 
 
@@ -80,4 +89,30 @@ while player_score <target_score and computer_score <target_score:
    elif computer_score>=target_score:     #otherwise the computer win 
     print("The computer win:"+str(computer_score)) #Print the computer is win. 
     
+"""
+
+"""
+import random  # Import random libary
+def rolls(): # function, that responsible for rolling the dice. 
+  genrate=random.randint(1,6) # the genrate number will be store in genrate variable. 
+                              # Return the genrate number  
+  return genrate  #If we do not make return the value will be non. 
+print("The genrate number: ", rolls()) #if we want to see the number that been genrate we use print function. 
+
+#make the vairable 
+player_score=0 # for the player 
+computer_score=0 #The computer variable 
+target_score=20   #Variable for score 
+
+# while loop
+while player_score < target_score and computer_score < target_score: # so long the player_score is less than target_score and computer_score is less than target score
+  player_score+=rolls() # this code is calling the rolls function and opdating the player_score 
+  computer_score+=rolls() #The code is doing the same thing. calling the function and adding the value to computer_score 
+  if player_score>=target_score: #Condition, the code is if the player_score is greater or equal to target_score 
+    print("The player is win", str(player_score)) #That mean the player is will win
+    break #If the player win the loop will end 
+  elif computer_score>=target_score: # otherwise the computer is greater or equal to target_score 
+   print("The computer is win: ",str(computer_score)) #The computer will win. cl
+  
+
 """
